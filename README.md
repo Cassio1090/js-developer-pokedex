1ceba8810caf847f7bdfcc0027176da0bbc8694c
Cassio1090 <cassio-byob@hotmail.com>
2023-09-14 18:00:29 -0400

    PokeAPi modificada 0.1

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# Trilha JS Developer - Pokedex
+# Projeto Pokedex API | Cassio1090
+
+O projeto inicio no bootcamp da DIO, criando uma Pokedex API, usando css, javascript e html.
+
+
+
+
+
+
+
+
+## Habilidades
+![GitHub](https://img.shields.io/badge/GitHub-000?style=for-the-badge&logo=github&logoColor=)
+![Git](https://img.shields.io/badge/GitHub-000?style=for-the-badge&logo=git&logoColor=)
+![HTML5](https://img.shields.io/badge/HTML5-000?style=for-the-badge&logo=html5)
+![CSS3](https://img.shields.io/badge/CSS3-000?style=for-the-badge&logo=css3&logoColor=264CE4)
+![Python](https://img.shields.io/badge/Python-000?style=for-the-badge&logo=python)
+![C](https://img.shields.io/badge/C-000?style=for-the-badge&logo=c)
+
+## GitHub Status
+![GitHub Stats](https://github-readme-stats.vercel.app/api?username=Cassio1090&theme=aura)
+
+[![GitHub Streak](https://streak-stats.demolab.com/?user=Cassio1090&theme=aura)](https://git.io/streak-stats)
+
+## Referência
+
+ - [Awesome Readme Templates](https://awesomeopensource.com/project/elangosundar/awesome-README-templates)
+ - [Awesome README](https://github.com/matiassingers/awesome-readme)
+ - [How to write a Good readme](https://bulldogjob.com/news/449-how-to-write-a-good-readme-for-your-github-project)
--- assets/js/main.js
+++ assets/js/main.js
@@ -5,27 +5,22 @@ const maxRecords = 151
 const limit = 10
 let offset = 0;
 
-function convertPokemonToLi(pokemon) {
-    return `
-        <li class="pokemon ${pokemon.type}">
-            <span class="number">#${pokemon.number}</span>
-            <span class="name">${pokemon.name}</span>
-
-            <div class="detail">
-                <ol class="types">
-                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
-                </ol>
-
-                <img src="${pokemon.photo}"
-                     alt="${pokemon.name}">
-            </div>
-        </li>
-    `
-}
-
 function loadPokemonItens(offset, limit) {
-    pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
-        const newHtml = pokemons.map(convertPokemonToLi).join('')
+    pokeApi.getPokemons(offset, limit).then((pokemons = []) => { 
+        const newHtml = pokemons.map((pokemon) => `
+            <li class="pokemon ${pokemon.type}"> 
+                <span class="number">#${pokemon.number}</span>
+                <span class="name">${pokemon.name}</span>
+                
+                <div class="detail">
+                    <ol class="types">
+                        ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join ('')}
+                    </ol>
+                    <img src="${pokemon.photo}" alt="${pokemon.name}">
+                </div>
+                
+            </li>
+        `).join('')
         pokemonList.innerHTML += newHtml
     })
 }
@@ -34,14 +29,15 @@ loadPokemonItens(offset, limit)
 
 loadMoreButton.addEventListener('click', () => {
     offset += limit
-    const qtdRecordsWithNexPage = offset + limit
+    const qtdRecordWithNextPage = offset + limit
 
-    if (qtdRecordsWithNexPage >= maxRecords) {
+    if (qtdRecordWithNextPage >= maxRecords){
         const newLimit = maxRecords - offset
         loadPokemonItens(offset, newLimit)
 
         loadMoreButton.parentElement.removeChild(loadMoreButton)
     } else {
         loadPokemonItens(offset, limit)
-    }
+      
+    }    
 })
